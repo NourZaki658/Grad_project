1c434fd2d0647433ebc82b700d61bc826d66de91
Nour Mohamed <nournmohamed586@gmail.com>
2026-05-18 12:11:00 +0300

    fix github pages router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./home/Home";
 import DoctorRegister from "./pages/DoctorRegister";
 import DoctorDashboard from "./pages/DoctorDashboard" ;
